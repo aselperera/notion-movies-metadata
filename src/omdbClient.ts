426f554f5c0c6f6a137fd012c7aec3969680deb1
aselperera <aselperera@hotmail.com>
2024-11-10 20:25:26 +0000

    Set up OMDB client

---
+++ src/omdbClient.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const omdbApiKey = process.env.OMDB_API_KEY;
+
+export const getMetadata = async (title: string, year: string) => {
+	const url = `http://www.omdbapi.com/?apikey=${omdbApiKey}&t=${title}&y=${year}`;
+	const response = await axios.get(url);
+	return response.data;
+};
+
+getMetadata('Inception', '2010').then(console.log);
